04965097f43b071376a1bb48aae025252c9bbe5c
Farhan M <farhan.mry@gmail.com>
2019-08-05 00:02:30 +0700

    new file:   SpotyBot/config.py

---
+++ SpotyBot/config.py
@@ -0,0 +1,7 @@
+import os
+CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
+CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
+
+CLIENTID = os.environ.get('CLIENTID')
+CLIENTSECRET = os.environ.get('CLIENTSECRET')
+
